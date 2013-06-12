15b4a3370e3c8b408ff5f70a161d19a2c9994bed
Jack Reedgit config --global user.name Jack <phillipjreed@gmail.com>
2013-06-12 14:29:26 -0400

    revert to original method of finding country

--- alumnimap/js/map.js
+++ alumnimap/js/map.js
@@ -1,22 +1,50 @@
 //Declare global variables
 var map;
 var popup;
-// var cmk = "315bfb243f9f4e8fb6adc193a7367eeb";
+
+
+//PanInsideBounds Fix
+L.Map.include({
+    panInsideBounds: function(bounds) {
+        bounds = L.latLngBounds(bounds);
+
+        var viewBounds = this.getBounds(),
+            viewSw = this.project(viewBounds.getSouthWest()),
+            viewNe = this.project(viewBounds.getNorthEast()),
+            sw = this.project(bounds.getSouthWest()),
+            ne = this.project(bounds.getNorthEast()),
+            dx = 0,
+            dy = 0;
+
+        if (viewNe.y < ne.y) { // north
+            dy = ne.y - viewNe.y + Math.max(0, this.latLngToContainerPoint([85.05112878, 0]).y); // + extra vertical scroll
+        }
+        if (viewNe.x > ne.x) { // east
+            dx = ne.x - viewNe.x;
+        }
+        if (viewSw.y > sw.y) { // south
+            dy = sw.y - viewSw.y + Math.min(0, this.latLngToContainerPoint([-85.05112878, 0]).y - this.getSize().y); // + extra vertical scroll
+        }
+        if (viewSw.x < sw.x) { // west
+            dx = sw.x - viewSw.x;
+        }
+
+        return this.panBy(new L.Point(dx, dy, true));
+    }
+});
 
 
 //Setup Map
 map = L.map('map', {
-	maxZoom:6
+	maxZoom: 6
 }).setView([0, 0], 2);
 L.tileLayer('tiles/map2/{z}/{x}/{y}.png', {}).addTo(map);
-// L.tileLayer('http://c.tiles.mapbox.com/v3/examples.map-vyofok3q/{z}/{x}/{y}.png', {
-// 	attribution: 'Map data &copy; 2011 OpenStreetMap contributors',
-// 	key: cmk
-// }).addTo(map);
+
 
 map.setMaxBounds([
-	[-180, -1800],
-	[180, 1800]
+
+	[-85, -180.0],
+	[85, 180.0]
 ]);
 
 
@@ -28,55 +56,23 @@ var nullStyle = {
 };
 
 //Popup for each country
-// function onEachCountry(feature, layer) {
-// 	var popupContent = feature.properties.iso_a3;
-// 	layer.bindPopup(popupContent);
-// }
+function onEachCountry(feature, layer) {
+	var popupContent = feature.properties.iso_a3;
+	layer.bindPopup(popupContent);
+}
 
 //On click of the country.  Right now just writes the iso_a3 property to the console
-// function onCountryClick(e) {
-// 	console.log(e.layer.feature.properties.iso_a3);
-// }
-
-
-//Looks up country from OpenStreetMap Nominatim database web service.  Returns two letter country code
-function lookupCountry(e) {
-	var lat = e.latlng.lat
-	var lng = e.latlng.lng
-	while (lng > 180) {
-		console.log('uh oh')
-		lng = lng - 360
-	}
-	while (lng < -180) {
-		lng = lng + 360
-	}
-	$.ajax({
-		dataType: 'json',
-		url: 'http://nominatim.openstreetmap.org/reverse?format=json&limit=1&lat=' + lat + '&lon=' + lng + '&zoom=10&addressdetails=1'
-
-	}).done(function(data) {
-		if (data.error) {
-			console.log(data.error)
-		} else {
-			popup.setContent(data.address.country_code).openOn(map)
-			console.log(data.address.country_code)
-		}
-	})
+function onCountryClick(e) {
+	console.log(e.layer.feature.properties.iso_a3);
 }
 
 //Loads geoJSON file containing country boundaries
-// $.getJSON('js/countries.geojson', function(data) {
-// 	countries = L.geoJson(data, {
-// 		style: nullStyle,
-// 		onEachFeature: onEachCountry
-// 	}).addTo(map)
-// 		.on('click', function(e) {
-// 		onCountryClick(e);
-// 	});
-// });
-
-map.on('click', function(e) {
-	lookupCountry(e)
-	popup = L.popup()
-		.setLatLng(e.latlng);
-})
+$.getJSON('js/countries.geojson', function(data) {
+	countries = L.geoJson(data, {
+		style: nullStyle,
+		onEachFeature: onEachCountry
+	}).addTo(map)
+		.on('click', function(e) {
+		onCountryClick(e);
+	});
+});
